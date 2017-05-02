32b3a448a2794f6c08ef49bd2a8185d3efdfe54b
Sean Hill <sean_hill@vancity.com>
2017-05-02 15:37:51 -0700

    Updating web app text

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ signal.signal(signal.SIGINT, lambda s, f: os._exit(0))
 
 @app.route("/")
 def generate_buzz():
-    page = '<html><body><h1>'
+    page = '<html><body><h1>Buzz: '
     page += generator.generate_buzz()
     page += '</h1></body></html>'
     return page
